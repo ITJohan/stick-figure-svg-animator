656c9099e3b580d280e667f2bd2fc4dd9e7b9dec
Johan Lindkvist <johanlindkvist89@gmail.com>
2023-01-17 20:28:51 +0100

    add movable red dots

--- public/script.js
+++ public/script.js
@@ -2,72 +2,44 @@ const bodyElement = document.querySelector('body');
 const svgElement = document.querySelector('svg');
 
 /**
- * @param { number } id
- * @param {{ x: number, y: number }} point
+ * @param {{ x: number, y: number, isActive: boolean }} point
  * @param {{ partId: string, xAttribute: string, yAttribute: string }} elementData
  */
-const setupPoint = (id, point, elementData) => {
-  // Set up HTML
-  const textElement = document.createElementNS(
+const setupPoint = (point, elementData) => {
+  // Setup elements
+  const pointElement = document.createElementNS(
     'http://www.w3.org/2000/svg',
-    'text'
+    'circle'
   );
-  const xLabelElement = document.createElement('label');
-  const yLabelElement = document.createElement('label');
-  const xInputElement = document.createElement('input');
-  const yInputElement = document.createElement('input');
-
-  // Initialize values
-  textElement.textContent = id;
-  textElement.setAttribute('x', point.x);
-  textElement.setAttribute('y', point.y);
-
-  xLabelElement.setAttribute('for', `x-${id}-start`);
-  yLabelElement.setAttribute('for', `y-${id}-start`);
-  xLabelElement.innerText = `x ${id} start`;
-  yLabelElement.innerText = `y ${id} start`;
-
-  xInputElement.setAttribute('id', `x-${id}-start`);
-  yInputElement.setAttribute('id', `y-${id}-start`);
-  xInputElement.setAttribute('type', 'range');
-  yInputElement.setAttribute('type', 'range');
-  xInputElement.setAttribute('min', '0');
-  yInputElement.setAttribute('min', '0');
-  xInputElement.setAttribute('max', '200');
-  yInputElement.setAttribute('max', '200');
-  xInputElement.setAttribute('step', '1');
-  yInputElement.setAttribute('step', '1');
-  xInputElement.setAttribute('value', point.x);
-  yInputElement.setAttribute('value', point.y);
-
+  pointElement.classList.add('point');
+  pointElement.setAttribute('cx', point.x);
+  pointElement.setAttribute('cy', point.y);
+  pointElement.setAttribute('r', '4');
   elementData.forEach((data) => {
     const element = document.querySelector(data.partId);
     element.setAttribute(data.xAttribute, point.x);
     element.setAttribute(data.yAttribute, point.y);
   });
 
-  // Set up event listeners
-  xInputElement.addEventListener('input', (e) => {
-    elementData.forEach((data) => {
-      const element = document.querySelector(data.partId);
-      element.setAttribute(data.xAttribute, e.target.value);
-    });
-    textElement.setAttribute('x', e.target.value);
-  });
-  yInputElement.addEventListener('input', (e) => {
-    elementData.forEach((data) => {
-      const element = document.querySelector(data.partId);
-      element.setAttribute(data.yAttribute, e.target.value);
-    });
-    textElement.setAttribute('y', e.target.value);
-  });
+  // Event listeners
+  pointElement.addEventListener('mousedown', () => (point.isActive = true));
+  pointElement.addEventListener('mousemove', (e) => {
+    console.log(e);
+    if (point.isActive) {
+      elementData.forEach((data) => {
+        const element = document.querySelector(data.partId);
+        element.setAttribute(data.xAttribute, e.offsetX);
+        element.setAttribute(data.yAttribute, e.offsetY);
+      });
 
-  // Add elements to html
-  svgElement.appendChild(textElement);
-  bodyElement.appendChild(xLabelElement);
-  bodyElement.appendChild(xInputElement);
-  bodyElement.appendChild(yLabelElement);
-  bodyElement.appendChild(yInputElement);
+      pointElement.setAttribute('cx', e.offsetX);
+      pointElement.setAttribute('cy', e.offsetY);
+    }
+  });
+  pointElement.addEventListener('mouseup', () => (point.isActive = false));
+
+  // Add to SVG
+  svgElement.appendChild(pointElement);
 };
 
 // SVG parts
@@ -83,50 +55,46 @@ const rightUpperLeg = document.querySelector('#right-upper-leg');
 const leftLowerLeg = document.querySelector('#left-lower-leg');
 const rightLowerLeg = document.querySelector('#right-lower-leg');
 
-setupPoint(1, { x: 100, y: 16 }, [
+setupPoint({ x: 100, y: 16, isActive: false }, [
   { partId: '#head', xAttribute: 'cx', yAttribute: 'cy' },
   { partId: '#neck', xAttribute: 'x1', yAttribute: 'y1' },
 ]);
-setupPoint(2, { x: 100, y: 40 }, [
+setupPoint({ x: 100, y: 40 }, [
   { partId: '#neck', xAttribute: 'x2', yAttribute: 'y2' },
   { partId: '#body', xAttribute: 'x1', yAttribute: 'y1' },
   { partId: '#left-upper-arm', xAttribute: 'x1', yAttribute: 'y1' },
   { partId: '#right-upper-arm', xAttribute: 'x1', yAttribute: 'y1' },
 ]);
-setupPoint(3, { x: 80, y: 80 }, [
+setupPoint({ x: 80, y: 80 }, [
   { partId: '#left-upper-arm', xAttribute: 'x2', yAttribute: 'y2' },
   { partId: '#left-lower-arm', xAttribute: 'x1', yAttribute: 'y1' },
 ]);
-setupPoint(4, { x: 120, y: 80 }, [
+setupPoint({ x: 120, y: 80 }, [
   { partId: '#right-upper-arm', xAttribute: 'x2', yAttribute: 'y2' },
   { partId: '#right-lower-arm', xAttribute: 'x1', yAttribute: 'y1' },
 ]);
-setupPoint(5, { x: 100, y: 104 }, [
+setupPoint({ x: 100, y: 104 }, [
   { partId: '#body', xAttribute: 'x2', yAttribute: 'y2' },
   { partId: '#left-upper-leg', xAttribute: 'x1', yAttribute: 'y1' },
   { partId: '#right-upper-leg', xAttribute: 'x1', yAttribute: 'y1' },
 ]);
-setupPoint(6, { x: 80, y: 104 }, [
+setupPoint({ x: 80, y: 104 }, [
   { partId: '#left-lower-arm', xAttribute: 'x2', yAttribute: 'y2' },
 ]);
-setupPoint(7, { x: 120, y: 104 }, [
+setupPoint({ x: 120, y: 104 }, [
   { partId: '#right-lower-arm', xAttribute: 'x2', yAttribute: 'y2' },
 ]);
-setupPoint(8, { x: 80, y: 124 }, [
+setupPoint({ x: 80, y: 124 }, [
   { partId: '#left-upper-leg', xAttribute: 'x2', yAttribute: 'y2' },
   { partId: '#left-lower-leg', xAttribute: 'x1', yAttribute: 'y1' },
 ]);
-setupPoint(8, { x: 80, y: 124 }, [
-  { partId: '#left-upper-leg', xAttribute: 'x2', yAttribute: 'y2' },
-  { partId: '#left-lower-leg', xAttribute: 'x1', yAttribute: 'y1' },
-]);
-setupPoint(9, { x: 120, y: 124 }, [
+setupPoint({ x: 120, y: 124 }, [
   { partId: '#right-upper-leg', xAttribute: 'x2', yAttribute: 'y2' },
   { partId: '#right-lower-leg', xAttribute: 'x1', yAttribute: 'y1' },
 ]);
-setupPoint(10, { x: 80, y: 160 }, [
+setupPoint({ x: 80, y: 160 }, [
   { partId: '#left-lower-leg', xAttribute: 'x2', yAttribute: 'y2' },
 ]);
-setupPoint(11, { x: 120, y: 160 }, [
+setupPoint({ x: 120, y: 160 }, [
   { partId: '#right-lower-leg', xAttribute: 'x2', yAttribute: 'y2' },
 ]);
