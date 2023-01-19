e835da9a02c20e56ee3574fb260051730a2185ff
Johan Lindkvist <johanlindkvist89@gmail.com>
2023-01-19 08:21:25 +0100

    add copy button

--- public/script.js
+++ public/script.js
@@ -2,11 +2,6 @@ const bodyElement = document.querySelector('body');
 const startSvg = document.querySelector('#start-svg');
 const endSvg = document.querySelector('#end-svg');
 
-/**
- * @param {{ x: number, y: number, isActive: boolean }} point
- * @param {{ partId: string, xAttribute: string, yAttribute: string }} elementData
- * @param { SVGElement } svg
- */
 const setupPoint = (point, elementData, svgElement) => {
   // Setup elements
   const pointElement = document.createElementNS(
@@ -17,6 +12,7 @@ const setupPoint = (point, elementData, svgElement) => {
   pointElement.setAttribute('cx', point.x);
   pointElement.setAttribute('cy', point.y);
   pointElement.setAttribute('r', '4');
+  pointElement.setAttribute('id', point.id);
   elementData.forEach((data) => {
     const element = document.querySelector(data.partId);
     element.setAttribute(data.xAttribute, point.x);
@@ -110,7 +106,7 @@ const setupPoints = (sideTag, svgSelector) => {
   const svgElement = document.querySelector(svgSelector);
 
   setupPoint(
-    { x: 100, y: 16, isActive: false },
+    { id: `${sideTag}-point-1`, x: 100, y: 16, isActive: false },
     [
       { partId: `#${sideTag}-head`, xAttribute: 'cx', yAttribute: 'cy' },
       { partId: `#${sideTag}-neck`, xAttribute: 'x1', yAttribute: 'y1' },
@@ -118,7 +114,7 @@ const setupPoints = (sideTag, svgSelector) => {
     svgElement
   );
   setupPoint(
-    { x: 100, y: 40, isActive: false },
+    { id: `${sideTag}-point-2`, x: 100, y: 40, isActive: false },
     [
       { partId: `#${sideTag}-neck`, xAttribute: 'x2', yAttribute: 'y2' },
       { partId: `#${sideTag}-body`, xAttribute: 'x1', yAttribute: 'y1' },
@@ -136,7 +132,7 @@ const setupPoints = (sideTag, svgSelector) => {
     svgElement
   );
   setupPoint(
-    { x: 80, y: 80, isActive: false },
+    { id: `${sideTag}-point-3`, x: 80, y: 80, isActive: false },
     [
       {
         partId: `#${sideTag}-left-upper-arm`,
@@ -152,7 +148,7 @@ const setupPoints = (sideTag, svgSelector) => {
     svgElement
   );
   setupPoint(
-    { x: 120, y: 80, isActive: false },
+    { id: `${sideTag}-point-4`, x: 120, y: 80, isActive: false },
     [
       {
         partId: `#${sideTag}-right-upper-arm`,
@@ -168,7 +164,7 @@ const setupPoints = (sideTag, svgSelector) => {
     svgElement
   );
   setupPoint(
-    { x: 100, y: 104, isActive: false },
+    { id: `${sideTag}-point-5`, x: 100, y: 104, isActive: false },
     [
       { partId: `#${sideTag}-body`, xAttribute: 'x2', yAttribute: 'y2' },
       {
@@ -185,7 +181,7 @@ const setupPoints = (sideTag, svgSelector) => {
     svgElement
   );
   setupPoint(
-    { x: 80, y: 104, isActive: false },
+    { id: `${sideTag}-point-6`, x: 80, y: 104, isActive: false },
     [
       {
         partId: `#${sideTag}-left-lower-arm`,
@@ -196,7 +192,7 @@ const setupPoints = (sideTag, svgSelector) => {
     svgElement
   );
   setupPoint(
-    { x: 120, y: 104, isActive: false },
+    { id: `${sideTag}-point-7`, x: 120, y: 104, isActive: false },
     [
       {
         partId: `#${sideTag}-right-lower-arm`,
@@ -207,7 +203,7 @@ const setupPoints = (sideTag, svgSelector) => {
     svgElement
   );
   setupPoint(
-    { x: 80, y: 124, isActive: false },
+    { id: `${sideTag}-point-8`, x: 80, y: 124, isActive: false },
     [
       {
         partId: `#${sideTag}-left-upper-leg`,
@@ -223,7 +219,7 @@ const setupPoints = (sideTag, svgSelector) => {
     svgElement
   );
   setupPoint(
-    { x: 120, y: 124, isActive: false },
+    { id: `${sideTag}-point-9`, x: 120, y: 124, isActive: false },
     [
       {
         partId: `#${sideTag}-right-upper-leg`,
@@ -239,7 +235,7 @@ const setupPoints = (sideTag, svgSelector) => {
     svgElement
   );
   setupPoint(
-    { x: 80, y: 160, isActive: false },
+    { id: `${sideTag}-point-10`, x: 80, y: 160, isActive: false },
     [
       {
         partId: `#${sideTag}-left-lower-leg`,
@@ -250,7 +246,7 @@ const setupPoints = (sideTag, svgSelector) => {
     svgElement
   );
   setupPoint(
-    { x: 120, y: 160, isActive: false },
+    { id: `${sideTag}-point-11`, x: 120, y: 160, isActive: false },
     [
       {
         partId: `#${sideTag}-right-lower-leg`,
@@ -264,3 +260,42 @@ const setupPoints = (sideTag, svgSelector) => {
 
 setupPoints('start', '#start-svg');
 setupPoints('end', '#end-svg');
+
+document.querySelector('button').addEventListener('click', () => {
+  const startSvg = document.querySelector('#start-svg');
+  startSvg.childNodes.forEach((node) => {
+    if (node.nodeName === 'circle') {
+      const xAttribute = node.getAttribute('cx');
+      const yAttribute = node.getAttribute('cy');
+
+      const endSelector = `#end-${node
+        .getAttribute('id')
+        .split('-')
+        .slice(1)
+        .join('-')}`;
+
+      const endElement = document.querySelector(endSelector);
+      endElement.setAttribute('cx', xAttribute);
+      endElement.setAttribute('cy', yAttribute);
+    }
+
+    if (node.nodeName === 'line') {
+      const x1Attribute = node.getAttribute('x1');
+      const y1Attribute = node.getAttribute('y1');
+      const x2Attribute = node.getAttribute('x2');
+      const y2Attribute = node.getAttribute('y2');
+
+      const endSelector = `#end-${node
+        .getAttribute('id')
+        .split('-')
+        .slice(1)
+        .join('-')}`;
+
+      const endElement = document.querySelector(endSelector);
+      endElement.setAttribute('x1', x1Attribute);
+      endElement.setAttribute('y1', y1Attribute);
+      endElement.setAttribute('x2', x2Attribute);
+      endElement.setAttribute('y2', y2Attribute);
+    }
+  });
+});
